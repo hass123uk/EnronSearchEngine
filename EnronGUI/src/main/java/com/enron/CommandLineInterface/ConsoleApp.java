fa1a1952978ad04446ab2f7ee00cd560b9c9f8c6
Antal János Monori <anthonymonori@gmail.com>
2016-03-09 12:44:05 +0100

    removed old GUI module, init module cli

---
+++ EnronGUI/src/main/java/com/enron/CommandLineInterface/ConsoleApp.java
@@ -0,0 +1,12 @@
+package com.enron.CommandLineInterface;
+
+public class ConsoleApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
