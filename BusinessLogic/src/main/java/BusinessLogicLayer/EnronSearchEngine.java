b0a1b93be83df79ef8d68356ff2898502e26c366
Antal János Monori <anthonymonori@gmail.com>
2016-03-15 23:54:29 +0100

    Missed some BLL files, re-added missing mysql driver dependency, properly merged with master

---
+++ BusinessLogic/src/main/java/BusinessLogicLayer/EnronSearchEngine.java
@@ -0,0 +1,127 @@
+package BusinessLogicLayer;
+
+
+import Database.ContainsRepository;
+import Database.DocumentsRepository;
+import Database.TermsRepository;
+import FileSystem.FileLoader;
+import FileSystem.FileLoaderImpl;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static Database.Database.checkForTables;
+import static Database.Database.initDatabase;
+
+/**
+ *
+ */
+public class EnronSearchEngine {
+
+    private static final String HOME_DIR = "~";
+    private static final String FILE_NAME = "/EnronDataSet";
+
+    private static final String ALL_DOCS = "/MailDir_FullSet";
+    private static final String HALF_ALL_DOCS = "/MailDir_HalfSet";
+    private static final String FEW_DOCS = "/MailDir_SubSet";
+
+    private static final int DEFAULT_MAX_THREADS = 10;
+
+    private static final String ENRON_DATASET_DIR
+            = HOME_DIR
+            + FILE_NAME
+            + "/1000";
+
+    private static FileLoader fileLoader;
+    private static StringSplitter splitter;
+    private static ExecutorService pool;
+
+    private static TermsRepository termsRepository;
+    private static DocumentsRepository documentsRepository;
+    private static ContainsRepository containsRepository;
+    private static SynchronizedTermsMap synchronizedTermsMap;
+    private static IncrementalIDGenerator incrementalIDGenerator;
+
+    public static void main(String[] args) throws Exception {
+        long startTime = System.currentTimeMillis();
+
+        fileLoader = new FileLoaderImpl();
+        splitter = new StringSplitter("\\W+");
+
+        if (!checkForTables()) {
+            initDatabase();
+        }
+        incrementalIDGenerator = new IncrementalIDGenerator();
+        createRepositories();
+        pool = Executors.newWorkStealingPool(DEFAULT_MAX_THREADS);
+        synchronizedTermsMap = new SynchronizedTermsMap(termsRepository.selectAllTerms());
+        List<Callable<Void>> callables = loadFilesFromFSAndMapToCallables();
+        invokeAll(callables);
+
+        final long endTime = System.currentTimeMillis();
+        System.out.print("Total execution time: " + TimeUnit.MILLISECONDS.toMinutes(endTime - startTime)
+                + " minutes for " + callables.size() + " files.\n");
+        shutdownAndAwaitTermination(pool);
+    }
+
+    private static void createRepositories() {
+        termsRepository = new TermsRepository();
+        documentsRepository = new DocumentsRepository();
+        containsRepository = new ContainsRepository();
+    }
+
+    private static List<Callable<Void>> loadFilesFromFSAndMapToCallables() {
+        return fileLoader
+                .loadFiles(Paths.get(ENRON_DATASET_DIR.replaceFirst("^~", System.getProperty("user.home"))))
+                .stream()
+                .map(file -> newIndexFileTaskCallable(file.toPath()))
+                .collect(Collectors.toList());
+    }
+
+    private static Callable<Void> newIndexFileTaskCallable(Path filePath) {
+        return new IndexTaskCallable(filePath, incrementalIDGenerator,
+                synchronizedTermsMap, fileLoader, splitter, documentsRepository, termsRepository, containsRepository);
+    }
+
+    private static List<Void> invokeAll(List<Callable<Void>> indexingTasks) {
+        try {
+            return pool.invokeAll(indexingTasks)
+                    .stream()
+                    .map(future -> {
+                        try {
+                            return future.get();
+                        } catch (Exception e) {
+                            throw new IllegalStateException(e);
+                        }
+                    }).collect(Collectors.toList());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void shutdownAndAwaitTermination(ExecutorService pool) {
+        pool.shutdown(); // Disable new tasks from being submitted
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                pool.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!pool.awaitTermination(60, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            pool.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
+    }
+}
---
+++ BusinessLogic/src/main/java/BusinessLogicLayer/IndexTaskCallable.java
@@ -0,0 +1,95 @@
+package BusinessLogicLayer;
+
+import Database.ContainsRepository;
+import Database.DocumentsRepository;
+import Database.TermsRepository;
+import DomainModels.Document;
+import DomainModels.Term;
+import FileSystem.FileLoader;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class IndexTaskCallable implements Callable {
+
+    private final SynchronizedTermsMap synchronizedTermsMap;
+    private final Path filePath;
+    private final FileLoader fileLoader;
+    private final StringSplitter stringSplitter;
+    private final DocumentsRepository documentsRepository;
+    private final TermsRepository termsRepository;
+    private final ContainsRepository containsRepository;
+    private IncrementalIDGenerator incrementalIDGenerator;
+
+    public IndexTaskCallable(
+            Path filePath,
+            IncrementalIDGenerator incrementalIDGenerator, SynchronizedTermsMap synchronizedTermsMap,
+            FileLoader fileLoader,
+            StringSplitter stringSplitter,
+            DocumentsRepository documentsRepository,
+            TermsRepository termsRepository,
+            ContainsRepository containsRepository) {
+
+        this.filePath = filePath;
+        this.incrementalIDGenerator = incrementalIDGenerator;
+        this.synchronizedTermsMap = synchronizedTermsMap;
+        this.fileLoader = fileLoader;
+        this.stringSplitter = stringSplitter;
+        this.documentsRepository = documentsRepository;
+        this.termsRepository = termsRepository;
+        this.containsRepository = containsRepository;
+    }
+
+    @Override
+    public Void call() throws Exception {
+        List<String> lines = fileLoader.loadLines(filePath);
+
+        int documentId = incrementalIDGenerator.getNextDocumentID();
+        Document document = new Document(documentId, filePath.toAbsolutePath().toString(), new Date());
+        documentsRepository.insertDocument(document);
+
+        List<Integer> termIds = processAndBatchSaveTerms(stringSplitter.split(lines));
+
+        containsRepository.batchInsertContains(documentId, termIds);
+
+        return null;
+    }
+
+    private List<Integer> processAndBatchSaveTerms(List<String> termValues) {
+        List<Integer> termIds = new ArrayList<>();
+        List<Term> newTerms = new ArrayList<>();
+
+        int newTermID = incrementalIDGenerator.getNextTermID();
+        for (String termValue : termValues) {
+            int termID = getOrSetTermID(termValue, newTermID);
+            termIds.add(termID);
+
+            if (termID == newTermID) {
+                newTerms.add(new Term(termID, termValue));
+                newTermID = incrementalIDGenerator.getNextTermID();
+            }
+        }
+        termsRepository.batchInsertTerm(newTerms);
+        return termIds;
+    }
+
+    private int getOrSetTermID(String termValue, int newID) {
+        int termID;
+        synchronizedTermsMap.lock.lock();
+        try {
+            int idFromMap = synchronizedTermsMap.getTermIDIfPresent(termValue);
+            if (idFromMap != SynchronizedTermsMap.TERM_NOT_PRESENT) {
+                termID = idFromMap;
+            } else {
+                synchronizedTermsMap.putTerm(termValue, newID);
+                termID = newID;
+            }
+        } finally {
+            synchronizedTermsMap.lock.unlock();
+        }
+        return termID;
+    }
+}
